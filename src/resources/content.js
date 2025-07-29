9ad9925507847e940a29026d6645f1ee7823e2b0
Pankaj <pankaj2007acharjee@gmail.com>
2025-07-29 23:41:33 +0530

    Added bengali as a known language

--- src/resources/content.js
+++ src/resources/content.js
@@ -10,7 +10,7 @@ const person = {
   avatar: "/images/avatar.png",
   email: "pankaj2007acharjee@gmail.com",
   location: "Asia/Kolkata", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  languages: ["English", "Bengali"], 
 };
 
 const newsletter = {
